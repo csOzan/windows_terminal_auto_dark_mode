8ddf77668418e32760414fd6666527153b331bcf
Ozan Çakmak <ozan42.public@gmail.com>
2022-07-13 14:29:19 +0300

    initalize
    created core files

---
+++ runner.py
@@ -0,0 +1,11 @@
+import sys
+import tools
+
+def main():
+    if len(sys.argv) == 2:
+    # checking if user wants to change settings
+        return
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools.py
@@ -0,0 +1,21 @@
+import os
+
+
+def settings_first_configure():
+    pass
+
+
+def settings_change():
+    pass
+
+
+def settings_check():
+    pass
+
+# JSON TOOLS
+
+def is_available(json_name, json_path):
+#   A function to check availablity of given json file
+    pass
+
+def 
